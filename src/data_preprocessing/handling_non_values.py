c2bf648f03592d3464eae5ca99a7740b738b09d2
Hanif.Emamgholizadeh <hanif.emamgholizadeh@unibz.it>
2024-12-08 14:08:12 +0100

    preprocessing, nan values, none variance updated last

--- src/data_preprocessing/handling_non_values.py
+++ src/data_preprocessing/handling_non_values.py
@@ -4,35 +4,28 @@ from data_object import Data
 
 
 def handle_nan(data_train: Data, data_test: Data, nan_threshold=0.5):
-    # TODO add other imputing methods instead of median
-    numeric_columns = data_train.numerical_columns
-    numeric_columns, _ = remove_high_nan_features(
-        data_train, data_test, numeric_columns, nan_threshold
-    )
-    handle_numerical_nan(data_train, data_test, numeric_columns, nan_threshold)
+    removed_column_info = remove_high_nan_features(data_train, data_test, nan_threshold)
+    handle_numerical_nan(data_train, data_test, nan_threshold)
+    handle_categorical_nan(data_train, data_test, nan_threshold)
+    return removed_column_info
 
+
+def handle_numerical_nan(data_train: Data, data_test: Data):
+    numeric_columns = data_train.numerical_columns
+    for column in numeric_columns:
+        median_value = data_train.features[column].median()
+        set_values_for_numeric_nan(data_train, column, median_value)
+        set_values_for_numeric_nan(data_test, column, median_value)
+
+
+def set_values_for_numeric_nan(data: Data, column, median_value):
+    nan_indices = data.features[column].isna().index.tolist()
+    data.set_new_values(column, nan_indices, median_value)
+
+
+def handle_categorical_nan(data_train: Data, data_test: Data):
     cat_bool_columns = data_train.boolean_columns
     cat_bool_columns.extend(data_train.categorical_columns)
-    cat_bool_columns, _ = remove_high_nan_features(
-        data_train, data_test, cat_bool_columns, nan_threshold
-    )
-    handle_categorical_nan(data_train, data_test, cat_bool_columns, nan_threshold)
-
-
-def handle_numerical_nan(
-    data_train: Data, data_test: Data, numeric_columns, nan_threshold
-):
-    for column in numeric_columns:
-        if data_train.features[column].isnull().any():
-            median_value = data_train.features[column].median()
-            nan_indices = data_train.features[column].isna().index.tolist()
-            data_train.set_new_values(column, nan_indices, median_value)
-
-
-def handle_categorical_nan(
-    data_train: Data, data_test: Data, cat_bool_columns, nan_threshold
-):
-
     for column in cat_bool_columns:
         if data_train.features[column].isnull().any():
             new_category = "New_category"
@@ -42,27 +35,24 @@ def handle_categorical_nan(
             data_train.set_new_values(column, nan_indices, new_category)
 
 
-def remove_high_nan_features(
-    data_train: Data, data_test: Data, columns_name, nan_threshold
-):
+def remove_high_nan_features(data_train: Data, data_test: Data, nan_threshold):
+    columns_name = data_train.features.columns
     nan_proportions = data_train.features[columns_name].isna().mean()
 
-    columns_to_keep, removed_columns_info = get_removed_kept_columns(
+    removed_columns_info = removed_columns_info(
         data_train, data_test, nan_proportions, nan_threshold, columns_name
     )
 
-    return columns_to_keep, removed_columns_info
+    return removed_columns_info
 
 
-def get_removed_kept_columns(
-    data_train: Data, data_test: Data, nan_proportions, nan_threshold, columns_name
+def removed_columns_info(
+    data_train: Data, data_test: Data, nan_proportions, nan_threshold
 ):
     if nan_threshold is not None:
         columns_to_remove = nan_proportions[nan_proportions > nan_threshold].index
-        columns_to_keep = nan_proportions[nan_proportions <= nan_threshold].index
     else:
         columns_to_remove = []
-        columns_to_keep = columns_name
     removed_columns_info = pd.DataFrame(
         {
             "column": columns_to_remove,
@@ -71,4 +61,4 @@ def get_removed_kept_columns(
     )
     data_train.remove_columns(columns_to_remove)
     data_test.remove_columns(columns_to_remove)
-    return columns_to_keep, removed_columns_info
+    return removed_columns_info
